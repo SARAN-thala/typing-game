80e20832975120824b87af99b3e9e7a15ca83b72
Saran <saranrajmalar@gmail.com>
2016-11-19 13:06:04 +0530

    Saran | #refactor some of the methods for tweaking speed
    - some changes on css for better visual

--- typingGame.js
+++ typingGame.js
@@ -1,101 +1,4 @@
-// var temp = document.querySelector('.time');
-// var button = document.querySelector("button");
-// var words = document.querySelector(".words");
-// var timerDiv = document.querySelector(".time");
-// var scoreDiv = document.querySelector(".score");
-// var points = 0;
-// var spans;
-// var typed;
-// var seconds = 60;
-// var spark = new Audio("spark.mp3");
-//
-// function countdown() {
-//     points = 0;
-//     var timer = setInterval(function () {
-//         button.disabled = true;
-//         seconds--;
-//         temp.innerHTML = seconds;
-//         if (seconds === 0) {
-//             alert("Game over! Your score is " + points);
-//             scoreDiv.innerHTML = "0";
-//             words.innerHTML = "";
-//             button.disabled = false;
-//             clearInterval(timer);
-//             seconds = 60;
-//             timerDiv.innerHTML = "60";
-//             button.disabled = false;
-//         }
-//     }, 1000);
-// }
-//
-// function random() {
-//     words.innerHTML = "";
-//     var random = Math.floor(Math.random() * (1943 - 0 + 1)) + 0; // random number of 1943 words in the list
-//     var wordArray = list[random].split("");
-//     for (var i = 0; i < wordArray.length; i++) { //building the words with spans around the letters
-//         var span = document.createElement("span");
-//         span.classList.add("span");
-//         span.innerHTML = wordArray[i];
-//         words.appendChild(span);
-//     }
-//     spans = document.querySelectorAll(".span");
-// }
-//
-// const list = [HERE IS A LIST OF 1900 + WORDS, so i cut it to save the space];
-//
-//
-// button.addEventListener("click", function (e) {
-//     countdown();
-//     random();
-//     button.disabled = true;
-// });
-//
-//
-// function typing(e) {
-//     typed = String.fromCharCode(e.which);
-//     for (var i = 0; i < spans.length; i++) {
-//         if (spans[i].innerHTML === typed) { // if typed letter is the one from the word
-//             if (spans[i].classList.contains("bg")) { // if it already has class with the bacground color then check the next one
-//                 continue;
-//             } else if (spans[i].classList.contains("bg") === false && spans[i - 1] === undefined || spans[i - 1].classList.contains("bg") !== false) { // if it dont have class, if it is not first letter or if the letter before it dont have class (this is done to avoid marking the letters who are not in order for being checked, for example if you have two "A"s so to avoid marking both of them if the first one is at the index 0 and second at index 5 for example)
-//                 spans[i].classList.add("bg");
-//                 break;
-//             }
-//         }
-//     }
-//     var checker = 0;
-//     for (var j = 0; j < spans.length; j++) { //checking if all the letters are typed
-//         if (spans[j].className === "span bg") {
-//             checker++;
-//         }
-//         if (checker === spans.length) { // if so, animate the words with animate.css class
-//             spark.pause();
-//             spark.currentTime = 0;
-//             spark.play();
-//             words.classList.add("animated");
-//             words.classList.add("fadeOut");
-//             points++; // increment the points
-//             scoreDiv.innerHTML = points; //add points to the points div
-//             document.removeEventListener("keydown", typing, false);
-//             setTimeout(function () {
-//                 words.className = "words"; // restart the classes
-//                 random(); // give another word
-//                 document.addEventListener("keydown", typing, false);
-//             }, 400);
-//         }
-//
-//     }
-// }
-//
-// button.addEventListener("click", function (e) {
-//     countdown();
-//     random();
-//     button.disabled = true;
-//     document.addEventListener("keydown", typing, false);
-// });
-//----------------------------------------
-
-var temp = document.querySelector('.time');
+var temp = document.querySelector(".time");
 var button = document.querySelector("button");
 var words = document.querySelector(".words");
 var timerDiv = document.querySelector(".time");
@@ -108,34 +11,39 @@ var spark = new Audio("http://k003.kiwi6.com/hotlink/qdpr7bioht/spark.mp3");
 
 function countdown() {
     points = 0;
-    var timer = setInterval(function () {
-        button.disabled = true;
-        seconds--;
-        temp.innerHTML = seconds;
-        if (seconds === 0) {
-            alert("Game over! Your score is " + points);
-            scoreDiv.innerHTML = "0";
-            words.innerHTML = "";
-            button.disabled = false;
-            clearInterval(timer);
-            seconds = 60;
-            timerDiv.innerHTML = "60";
-            button.disabled = false;
-        }
-    }, 1000);
+    setTimeout(update, 1000);
 }
 
-function random() {
+function update() {
+    button.disabled = true;
+    seconds--;
+    temp.innerHTML = seconds;
+    if (seconds === 0) {
+        alert("Game over! Your score is " + points);
+        scoreDiv.innerHTML = "0";
+        words.innerHTML = "";
+        button.disabled = false;
+        seconds = 60;
+        timerDiv.innerHTML = "60";
+        button.disabled = false;
+    }
+    else {
+        setTimeout(update, 1000);
+    }
+}
+
+function getNewWord() {
     words.innerHTML = "";
-    var random = Math.floor(Math.random() * (1943 - 0 + 1)) + 0;
+    var random = Math.floor(Math.random() * (1944));
     var wordArray = list[random].split("");
-    for (var i = 0; i < wordArray.length; i++) { //building the words with spans around the letters
+    spans = [];
+    for (var i = 0; i < wordArray.length; i++) {    //building the words with spans around the letters
         var span = document.createElement("span");
         span.classList.add("span");
         span.innerHTML = wordArray[i];
         words.appendChild(span);
+        spans.push(span);
     }
-    spans = document.querySelectorAll(".span");
 }
 
 
@@ -383,19 +291,19 @@ const list = ['ACCOUNT', 'ACCURATE', 'ACRES', 'ACROSS', 'ACT', 'ACTION', 'ACTIVE
     'YEAR', 'YELLOW', 'YES', 'YESTERDAY', 'YET', 'YOU', 'YOUNG', 'YOUNGER',
     'YOUR', 'YOURSELF', 'YOUTH', 'ZERO', 'ZOO'];
 
-button.addEventListener("click", function (e) {
+button.addEventListener("click", function () {
     countdown();
-    random();
+    getNewWord();
     button.disabled = true;
 });
 
 
-function typing(e) {
+function checkKey(e) {
     typed = String.fromCharCode(e.which);
     for (var i = 0; i < spans.length; i++) {
-        if (spans[i].innerHTML === typed) { // if typed letter is the one from the word
-            if (spans[i].classList.contains("bg")) { // if it already has class with the bacground color then check the next one
-                continue;
+        if (spans[i].innerHTML === typed) {   // if typed letter is the one from the word
+            if (spans[i].classList.contains("bg")) {   // if it already has class with the bacground color then check the next one
+
             } else if (spans[i].classList.contains("bg") === false && spans[i - 1] === undefined || spans[i - 1].classList.contains("bg") !== false) { // if it dont have class, if it is not first letter or if the letter before it dont have class (this is done to avoid marking the letters who are not in order for being checked, for example if you have two "A"s so to avoid marking both of them if the first one is at the index 0 and second at index 5 for example)
                 spans[i].classList.add("bg");
                 break;
@@ -403,27 +311,26 @@ function typing(e) {
         }
     }
     var checker = 0;
-    for (var j = 0; j < spans.length; j++) { //checking if all the letters are typed
+    for (var j = 0; j < spans.length; j++) {   //checking if all the letters are typed
         if (spans[j].className === "span bg") {
             checker++;
         }
-        if (checker === spans.length) { // if so, animate the words with animate.css class
+        if (checker === spans.length) {   // if so, animate the words with animate.css class
             spark.pause();
             spark.currentTime = 0;
             spark.play();
             words.classList.add("animated");
             words.classList.add("fadeOut");
-            points++; // increment the points
-            scoreDiv.innerHTML = points; //add points to the points div
-            document.removeEventListener("keydown", typing, false);
+            points++;   // increment the points
+            scoreDiv.innerHTML = points;   //add points to the points div
+            document.removeEventListener("keydown", checkKey, false);
             setTimeout(function () {
-                words.className = "words"; // restart the classes
-                random(); // give another word
-                document.addEventListener("keydown", typing, false);
+                words.className = "words";   // restart the classes
+                getNewWord();   // give another word
+                document.addEventListener("keydown", checkKey, false);
             }, 400);
         }
-
     }
 }
 
-document.addEventListener("keydown", typing, false);
+document.addEventListener("keydown", checkKey, false);
